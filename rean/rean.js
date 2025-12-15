ac249d78a0678fbc320eef5dc90c3f07b80c9ea9
Soth911 <puthpissoth@gmail.com>
2025-12-15 23:46:14 +0700

    update8

--- rean/rean.js
+++ rean/rean.js
@@ -3,3 +3,10 @@ const name = localStorage.getItem("userName");
     
 document.getElementById("welcomeText").textContent = "Hello, " + name + "!";
     
+    
+document.querySelectorAll("button[data-subject]").forEach(button => {
+    button.addEventListener("click", () => {
+        const subject = button.dataset.subject;
+        window.location.href = `../inclass/inclass.html?subject=${subject}`;
+    });
+});
